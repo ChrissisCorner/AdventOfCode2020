a0464ce6f1e9ffcdb7e7dad493b7491cea8325af
ChrissisCorner <christina.ulonska@mailbox.tu-dresden.de>
2020-12-18 19:52:33 +0100

    finished day04

--- day04.py
+++ day04.py
@@ -5,6 +5,7 @@ passports = []
 for p in passports_split:
     passports.append(p.replace('\n', ' ').split(' '))
 
+ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
 valid_passports = 0
 passports_final = []
 for p in passports:
@@ -13,12 +14,70 @@ for p in passports:
 
 for p in passports_final:
     if 'byr' in p.keys():
-        if 'iyr' in p.keys():
-            if 'eyr' in p.keys():
-                if 'hgt' in p.keys():
-                    if 'hcl' in p.keys():
-                        if 'ecl' in p.keys():
-                            if 'pid' in p.keys():
-                                valid_passports += 1
+        print(p.get('byr'))
+        if 1920 <= int(p.get('byr')) and int(p.get('byr')) <= 2002:
+            print('passed')
+            if 'iyr' in p.keys():
+                print('iyr: ', p.get('iyr'))
+                if 2010 <= int(p.get('iyr')) and int(p.get('iyr')) <= 2020:
+                    print('passed 2')
+                    if 'eyr' in p.keys():
+                        print('eyr: ', p.get('eyr'))
+                        if 2010 <= int(p.get('eyr')) and int(p.get('eyr')) <= 2030:
+                            print('passed 3')
+                            if 'hgt' in p.keys():
+                                print(p.get('hgt'))
+                                measure = p.get('hgt')[-2:]
+                                value = p.get('hgt')[:-2]
 
-print(valid_passports)
+                                if measure == 'in':
+                                    print('in')
+                                    if  59 <= int(value) and int(value) <= 76:
+                                        print('passed 4 -in')
+                                        if 'hcl' in p.keys():
+                                            print('hcl: ', p.get('hcl'))
+                                            #print(p.get('hcl')[:0])
+                                            #print(p.get('hcl')[:1])
+                                            if p.get('hcl')[:1] == '#':
+                                                print('passed #')
+                                                print('hcl: ', p.get('hcl')[1:])
+                                                if p.get('hcl')[1:].isalnum() and len(p.get('hcl')[1:]) == 6:
+                                                    print('passed 5')
+                                                    if 'ecl' in p.keys():
+                                                        if p.get('ecl') in ecl:
+                                                            print('passed 6')
+                                                            if 'pid' in p.keys():
+                                                                pid = p.get('pid')
+                                                                if len(pid) is 9:
+                                                                    print('length 9')
+                                                                    print(pid[:1])
+                                                                    if pid[:1] == '0':
+                                                                        print('passed 7')
+                                                                        valid_passports += 1
+
+                                elif measure == 'cm':
+                                    print('cm')
+                                    if 150 <= int(value) and int(value) <= 193:
+                                        print('passed 4 -cm')
+                                        if 'hcl' in p.keys():
+                                            print('hcl: ', p.get('hcl'))
+                                            # print(p.get('hcl')[:0])
+                                            # print(p.get('hcl')[:1])
+                                            if p.get('hcl')[:1] == '#':
+                                                print('passed #')
+                                                print('hcl: ', p.get('hcl')[1:])
+                                                if p.get('hcl')[1:].isalnum() and len(p.get('hcl')[1:]) == 6:
+                                                    print('passed 5')
+                                                    if 'ecl' in p.keys():
+                                                        if p.get('ecl') in ecl:
+                                                            print('passed 6')
+                                                            if 'pid' in p.keys():
+                                                                pid = p.get('pid')
+                                                                if len(pid) is 9:
+                                                                    print('length 9')
+                                                                    print(pid[:1])
+                                                                    if pid[:1] == '0':
+                                                                        print('passed 7')
+                                                                        valid_passports += 1
+print('Number passports', valid_passports)
+
